30482fcd724e0b90321bb094300c482e59740031
Markus Engelbrecht <github@enbre.com>
2015-02-01 15:10:23 +0100

    improve overview page

--- lib/Controllers/Overview.php
+++ lib/Controllers/Overview.php
@@ -5,9 +5,13 @@ namespace SimpleDAV\Controllers;
 use PicoFarad\Response;
 use PicoFarad\Router;
 use PicoFarad\Template;
+use SimpleDAV\Model\User;
 
 
 Router\get_action('overview', function() {
     Response\html(Template\load('overview', [
+        'values' => [
+            'username' => User::loggedIn()
+        ]
     ]));
 });
--- templates/overview.php
+++ templates/overview.php
@@ -10,37 +10,35 @@
     <title>Overview</title>
     <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.2/flatly/bootstrap.min.css" rel="stylesheet">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/overview.css" rel="stylesheet">
 </head>
 <body>
-<div class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <a href="?action=overview" class="navbar-brand">Overview</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">SimpleDAV</a>
         </div>
         <div class="navbar-collapse collapse navbar-inverse-collapse" id="navbar-main">
             <ul class="nav navbar-nav">
+                <li class="active"><a href="?action=overview"><i class="fa fa-user"></i> Overview</a></li>
                 <li><a href="?action=users"><i class="fa fa-users"></i> Manage Users</a></li>
-                <li><a href="?action=statistics"><i class="fa fa-server"></i> Statistics</a></li>
+                <li><a href="?action=statistics"><i class="fa fa-bar-chart"></i> Statistics</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="?action=logout">
-                        <i class="fa fa-sign-out"></i> Logout
-                    </a>
-                </li>
+                <li><a href="?action=logout"><i class="fa fa-sign-out"></i> Logout</a></li>
             </ul>
         </div>
     </div>
-</div>
+</nav>
 
 <div class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-8 col-md-7 col-sm-6">
-                <h1>Overview</h1>
-            </div>
-        </div>
-    </div>
+    <h3>User: <?= SimpleDAV\Utils::escape($values['username']) ?></h3>
 </div>
 </body>
 </html>
