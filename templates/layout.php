9a292fe02927ea13d11a0be3608df89f3b6c6dba
Markus Engelbrecht <github@enbre.com>
2015-02-07 10:39:16 +0100

    delete statistics page (will be integrated into admin overview)

--- templates/layout.php
+++ templates/layout.php
@@ -32,9 +32,6 @@
                 <li <?= isset($page) && $page === "users" ? 'class="active"' : '' ?>>
                     <a href="?action=users"><i class="fa fa-users"></i> Manage Users</a>
                 </li>
-                <li <?= isset($page) && $page === "statistics" ? 'class="active"' : '' ?>>
-                    <a href="?action=statistics"><i class="fa fa-bar-chart"></i> Statistics</a>
-                </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="navbar-text">Signed in as <?= SimpleDAV\Utils::escape($username) ?></li>
