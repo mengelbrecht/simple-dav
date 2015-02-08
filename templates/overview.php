9bd62343bdab93f2a11c0bab332e8bb1d965107a
Markus Engelbrecht <github@enbre.com>
2015-02-08 08:58:02 +0100

    improve overview page

--- templates/overview.php
+++ templates/overview.php
@@ -1,21 +1,42 @@
 <ul class="list-group">
     <li class="list-group-item active">
-        Calendars
+        <i class="fa fa-calendar"></i> Calendars
     </li>
+    <li class="list-group-item">
+        <a href="?action=addcalendar"><i class="fa fa-plus"></i> Create...</a>
+    </li>
+<?php if (empty($addressBooks)): ?>
+    <li class="list-group-item"><i>No address books</i></li>
+<?php else: ?>
 <?php foreach ($calendars as $name => $count): ?>
     <li class="list-group-item">
-        <?= SimpleDAV\Utils::escape($name) ?><span class="badge"><?= $count ?></span>
+        <div class="row">
+            <div class="col-md-5"><?= SimpleDAV\Utils::escape($name) ?></div>
+            <div class="col-md-5">Events <span class="badge"><?= $count ?></span></div>
+            <div class="col-md-2"><a href="#"><i class="fa fa-trash-o fa-lg"></i> Delete</a></div>
+        </div>
     </li>
 <?php endforeach ?>
+<?php endif ?>
 </ul>
-
 <ul class="list-group">
     <li class="list-group-item active">
-        Address Books
+        <i class="fa fa-book"></i> Address Books
     </li>
+    <li class="list-group-item">
+        <a href="?action=addaddressbook"><i class="fa fa-plus"></i> Create...</a>
+    </li>
+<?php if (empty($addressBooks)): ?>
+    <li class="list-group-item"><i>No address books</i></li>
+<?php else: ?>
 <?php foreach ($addressBooks as $name => $count): ?>
     <li class="list-group-item">
-        <?= SimpleDAV\Utils::escape($name) ?><span class="badge"><?= $count ?></span>
+        <div class="row">
+            <div class="col-md-5"><?= SimpleDAV\Utils::escape($name) ?></div>
+            <div class="col-md-5">Cards <span class="badge"><?= $count ?></span></div>
+            <div class="col-md-2"><a href="#"><i class="fa fa-trash-o fa-lg"></i> Delete</a></div>
+        </div>
     </li>
 <?php endforeach ?>
+<?php endif ?>
 </ul>
