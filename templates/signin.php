daa40ab7e045b349e7545a217ed3fc16987366ac
Markus Engelbrecht <github@enbre.com>
2015-02-12 20:36:59 +0100

    use form-group to simplify required css

--- templates/signin.php
+++ templates/signin.php
@@ -14,21 +14,26 @@
 <div class="container">
     <form class="form-signin" method="post" action="?action=login">
         <input type="hidden" name="csrf" id="form-csrf" value="<?= $values['csrf'] ?>"/>
-
         <h2 class="form-signin-heading">Please sign in</h2>
-        <input type="text" name="username" id=username" class="form-control" placeholder="Username" value="<?= SimpleDAV\Utils::escape($values['username']) ?>" required <?= isset($error) ? "" : "autofocus"?>>
-<?php if (isset($error)): ?>
-        <div class="form-group has-error has-feedback">
-<?php endif ?>
+        <div class="form-group">
+            <input type="text" name="username" id=username" class="form-control" placeholder="Username" value="<?= SimpleDAV\Utils::escape($values['username']) ?>" required <?= isset($error) ? "" : "autofocus"?>>
+        </div>
+        <div class="form-group <?= isset($error) ? "has-error has-feedback" : ""?>">
             <input class="form-control <?= isset($error) ? 'input-padding-right' : '' ?>" name="password" type="password" placeholder="Password" value="<?= isset($error) ? "" : SimpleDAV\Utils::escape($values['password']) ?>" required  <?= isset($error) ? "autofocus" : "" ?>>
 <?php if (isset($error)): ?>
             <span class="fa fa-exclamation-circle form-control-feedback input-suffix" aria-hidden="true"></span>
-        </div>
 <?php endif ?>
-        <div class="checkbox"><label><input type="checkbox" value="remember-me"> Remember me</label></div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit"><i class="fa fa-sign-in"></i> Sign in</button>
+        </div>
+        <div class="form-group">
+            <div class="checkbox"><label><input type="checkbox" value="remember-me"> Remember me</label></div>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-lg btn-primary btn-block" type="submit"><i class="fa fa-sign-in"></i> Sign in</button>
+        </div>
 <?php if (isset($error)): ?>
-        <div class="alert alert-danger"><i class="fa fa-exclamation-circle"></i> <?= SimpleDAV\Utils::escape($error) ?></div>
+        <div class="form-group">
+            <div class="alert alert-danger"><i class="fa fa-exclamation-circle"></i> <?= SimpleDAV\Utils::escape($error) ?></div>
+        </div>
 <?php endif ?>
     </form>
 </div>
